b8d635b63a1dd1dde8fddcd6adec09c580b20031
Shubham Patel <sp623702@gmail.com>
2026-05-03 07:38:08 +0530

    sum_of_two_numbers.cpp

---
+++ Basics/sum_of_two_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Problem: Sum of Two Numbers
+// Approach: Direct addition
+// Time Complexity: O(1)
+
+int main() {
+    int a = 10, b = 23;
+    cout << a + b;
+    return 0;
+}
