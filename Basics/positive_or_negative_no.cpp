7f19c4257eded72e82a85ab529d28b1ea3b5ef7d
Shubham Patel <sp623702@gmail.com>
2026-05-16 12:07:16 +0530

    positive_or_negative_no.cpp

---
+++ Basics/positive_or_negative_no.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout << "Enter a no." ;
+    cin >> n ;
+    if(n>=0){
+        cout<< " the number is a positine no." ;
+    }
+    else {
+        cout<< "the number is a negative no." ;
+    }
+}
